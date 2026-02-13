86ecaaa92a14d815cd46f9c210b7de0468e7bbe7
sanjeet2610 <195545915+sanjeet2610@users.noreply.github.com>
2026-02-13 18:58:16 +0000

    Show image and name of pokemon

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,7 @@
 export default function Card({ card, handleCardClick }) {
   return (
     <div onClick={() => handleCardClick(card.id)}>
+      <img src={card.image} alt={card.name} />
       <p>{card.name}</p>
     </div>
   );
